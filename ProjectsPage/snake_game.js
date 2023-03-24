17f35fb0579999fa8f18247f5fa88a41cee1c6ef
Logan Warren <51924513+LoganWarren@users.noreply.github.com>
2023-03-24 14:28:38 -0400

    Update snake_game.js

--- ProjectsPage/snake_game.js
+++ ProjectsPage/snake_game.js
@@ -47,38 +47,3 @@ function startGame() {
 }
 
 gameLoop(); // Call gameLoop() to start rendering the game
-
-
-function draw() {
-  context.fillStyle = 'white';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  if (gameState === "start") {
-    drawText("Press SPACE to Start", "20px Arial", "black", canvas.width / 2, canvas.height / 2);
-  } else if (gameState === "gameover") {
-    drawText("Game Over", "20px Arial", "black", canvas.width / 2, canvas.height / 2);
-    drawText("Press SPACE to Restart", "20px Arial", "black", canvas.width / 2, canvas.height / 2 + 30);
-  }
-
-  context.fillStyle = 'green';
-  snake.forEach(({ x, y }) => {
-    context.fillRect(x, y, gridSize, gridSize);
-  });
-
-  context.fillStyle = 'red';
-  context.fillRect(food.x, food.y, gridSize, gridSize);
-
-  requestAnimationFrame(draw);
-}
-
-function startGame() {
-  gameState = "playing";
-  snake = [{ x: gridSize * 5, y: gridSize * 5 }];
-  dx = gridSize;
-  dy = 0;
-  placeFood();
-  update(); // Call update() here to start the game loop when the game state changes to "playing"
-}
-
-update();
-draw();
