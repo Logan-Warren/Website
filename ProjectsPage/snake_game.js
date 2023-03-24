4491ace17c1e7e7535eeed71181c0d3a343b3c76
Logan Warren <51924513+LoganWarren@users.noreply.github.com>
2023-03-24 03:47:36 -0400

    Update snake_game.js

--- ProjectsPage/snake_game.js
+++ ProjectsPage/snake_game.js
@@ -10,6 +10,7 @@ let gameState = "start";
 
 function update() {
   if (gameState === "start" || gameState === "gameover") {
+    requestAnimationFrame(update);
     return;
   }
 
@@ -24,6 +25,7 @@ function update() {
 
   if (collision(head)) {
     gameState = "gameover";
+    requestAnimationFrame(update);
     return;
   }
 
@@ -32,53 +34,7 @@ function update() {
   }, snakeSpeed);
 }
 
-function draw() {
-  context.fillStyle = 'white';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  if (gameState === "start") {
-    drawText("Press SPACE to Start", "20px Arial", "black", canvas.width / 2, canvas.height / 2);
-  } else if (gameState === "gameover") {
-    drawText("Game Over", "20px Arial", "black", canvas.width / 2, canvas.height / 2);
-    drawText("Press SPACE to Restart", "20px Arial", "black", canvas.width / 2, canvas.height / 2 + 30);
-  }
-
-  context.fillStyle = 'green';
-  snake.forEach(({ x, y }) => {
-    context.fillRect(x, y, gridSize, gridSize);
-  });
-
-  context.fillStyle = 'red';
-  context.fillRect(food.x, food.y, gridSize, gridSize);
-
-  requestAnimationFrame(draw);
-}
-
-function placeFood() {
-  food = {
-    x: Math.floor(Math.random() * canvas.width / gridSize) * gridSize,
-    y: Math.floor(Math.random() * canvas.height / gridSize) * gridSize,
-  };
-}
-
-function drawText(text, font, color, x, y) {
-  context.font = font;
-  context.fillStyle = color;
-  context.textAlign = "center";
-  context.fillText(text, x, y);
-}
-
-function collision(head) {
-  return (
-    head.x < 0 ||
-    head.x >= canvas.width ||
-    head.y < 0 ||
-    head.y >= canvas.height ||
-    snake.slice(1).some(({ x, y }) => x === head.x && y === head.y)
-  );
-}
-
-function handleKeyDown(event) {
+document.addEventListener("keydown", (event) => {
   if (event.code === "Space") {
     if (gameState === "start" || gameState === "gameover") {
       startGame();
@@ -98,16 +54,7 @@ function handleKeyDown(event) {
       dy = 0;
     }
   }
-}
-
-function startGame() {
-  gameState = "playing";
-  snake = [{ x: gridSize * 5, y: gridSize * 5 }];
-  dx = gridSize;
-  dy = 0;
-  placeFood();
-  document.addEventListener("keydown", handleKeyDown); // Add this line
-}
+});
 
 update();
 draw();
